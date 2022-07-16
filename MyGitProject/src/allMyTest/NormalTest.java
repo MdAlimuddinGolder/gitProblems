221b91176602ef15b2eaa7510e01895373a80ecf
MdAlimuddinGolder <108498843+MdAlimuddinGolder@users.noreply.github.com>
2022-07-16 09:47:31 -0400

    I am trying to do some problem solve

--- MyGitProject/src/allMyTest/NormalTest.java
+++ MyGitProject/src/allMyTest/NormalTest.java
@@ -15,21 +15,26 @@ public class NormalTest {
 		obj.addition();
 		obj.localVariable();
 		obj.paramiter(20, 30);
+		NormalTest.Rohim();
+		String Return = obj.myAge();
+		System.out.println("What is the value of rteurn mathood =" +Return);
+		obj.funmethoid();
+		
 
 	}
 	public void Employee() {
 		int A = xy + yz;
 		int B = zx - xy;
-		System.out.println( "I want to see addition " +A);
-		System.out.println("Just Print out Subtraction " +B);
+		System.out.println( "I want to see addition =" +A);
+		System.out.println("Just Print out Subtraction =" +B);
 		System.out.println(name);
 		
 	}
 	public void addition() {
 		int AB = p + q;
 		int AC = xy + p;
-		System.out.println(" Pirnt out The Value of AB  " +AB);
-		System.out.println(" Show me the value of AC  " +AC);
+		System.out.println(" Pirnt out The Value of AB = " +AB);
+		System.out.println(" Show me the value of AC  =" +AC);
 	}
 	public void localVariable() {
 		int V = 12;
@@ -37,11 +42,32 @@ public class NormalTest {
 		int X = V+W;
 		String light = " What is your name";
 		System.out.println(+X);
-		System.out.println("What is light " +light);
+		System.out.println("What is light =" +light);
 	}
 	public void paramiter ( int a, int b) {
 		
 	int C = a + b;
-	System.out.println(" Print out Paramiter " +C);
+	System.out.println(" Print out Paramiter =" +C);
 	}
+	public static void Rohim () {
+		int Y = 20;
+		int d = 13;
+		int F = Y + d;
+		String www = "Bangladesh is a Small country";
+		System.out.println("I want to see static value =" +F );
+		System.out.println("String value ="+www);
+		
+	}
+	public String myAge () {
+		String name = "I am 25 years old";
+		return name;
+	}
+	public int funmethoid() {
+		int B = yz + q;
+		System.out.println("Return type data " +B);
+		return B;
+	}
+	
+	
+	
 }
